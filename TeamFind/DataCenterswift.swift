41356540e4899259a738f83bcb618d8045b3fbe4
SeongWoon Jeong <odysseyj@naver.com>
2019-04-18 18:08:14 +0900

    second

--- TeamFind/DataCenterswift.swift
+++ TeamFind/DataCenterswift.swift
@@ -24,16 +24,18 @@ struct Group {
 }
 
 
-let arrGroup : [Group] =
+var arrGroup : [Group] =
     [group1, group2, group3, group4, group5, group6, group7, group8, group9, group10]
 
-let group1 : Group = Group(title : "같이 밥먹을 사람!", location : "사근동", curPersonNumber : 3, maxPersonNumber : 5, endDate : "2019/04/29")
-let group2 : Group = Group(title : "같이 밥먹을 사람!", location : "사근동", curPersonNumber : 3, maxPersonNumber : 5, endDate : "2019/04/29")
-let group3 : Group = Group(title : "같이 밥먹을 사람!", location : "사근동", curPersonNumber : 3, maxPersonNumber : 5, endDate : "2019/04/29")
-let group4 : Group = Group(title : "같이 밥먹을 사람!", location : "사근동", curPersonNumber : 3, maxPersonNumber : 5, endDate : "2019/04/29")
-let group5 : Group = Group(title : "같이 밥먹을 사람!", location : "사근동", curPersonNumber : 3, maxPersonNumber : 5, endDate : "2019/04/29")
-let group6 : Group = Group(title : "같이 밥먹을 사람!", location : "사근동", curPersonNumber : 3, maxPersonNumber : 5, endDate : "2019/04/29")
-let group7 : Group = Group(title : "같이 밥먹을 사람!", location : "사근동", curPersonNumber : 3, maxPersonNumber : 5, endDate : "2019/04/29")
-let group8 : Group = Group(title : "같이 밥먹을 사람!", location : "사근동", curPersonNumber : 3, maxPersonNumber : 5, endDate : "2019/04/29")
-let group9 : Group = Group(title : "같이 밥먹을 사람!", location : "사근동", curPersonNumber : 3, maxPersonNumber : 5, endDate : "2019/04/29")
-let group10 : Group = Group(title : "같이 밥먹을 사람!", location : "사근동", curPersonNumber : 3, maxPersonNumber : 5, endDate : "2019/04/29")
+let group1 : Group = Group(title : "밥먹으실분 ", location : "사근동", curPersonNumber : 3, maxPersonNumber : 5, endDate : "2019/04/29")
+let group2 : Group = Group(title : "게임 하실분", location : "사근동", curPersonNumber : 7, maxPersonNumber : 10, endDate : "2019/04/29")
+let group3 : Group = Group(title : "놀러 가실분", location : "사근동", curPersonNumber : 3, maxPersonNumber : 5, endDate : "2019/04/29")
+let group4 : Group = Group(title : "과제 하실분", location : "사근동", curPersonNumber : 3, maxPersonNumber : 4, endDate : "2019/04/29")
+let group5 : Group = Group(title : "독서 하실분", location : "사근동", curPersonNumber : 1, maxPersonNumber : 30, endDate : "2019/04/29")
+let group6 : Group = Group(title : "꽃구경", location : "사근동", curPersonNumber : 3, maxPersonNumber : 20, endDate : "2019/04/29")
+let group7 : Group = Group(title : "볼링칠사람?", location : "사근동", curPersonNumber : 3, maxPersonNumber : 10, endDate : "2019/04/29")
+let group8 : Group = Group(title : "떡볶이파티", location : "사근동", curPersonNumber : 3, maxPersonNumber : 15, endDate : "2019/04/29")
+let group9 : Group = Group(title : "치킨머글사람", location : "사근동", curPersonNumber : 3, maxPersonNumber : 3, endDate : "2019/04/29")
+let group10 : Group = Group(title : "아이스크림파티", location : "사근동", curPersonNumber : 1, maxPersonNumber : 2, endDate : "2019/04/29")
+
+
---
+++ TeamFind/GroupTableViewController.swift
@@ -0,0 +1,100 @@
+//
+//  GroupTableViewController.swift
+//  TeamFind
+//
+//  Created by Jeong on 18/04/2019.
+//  Copyright © 2019 Jeong. All rights reserved.
+//
+
+import UIKit
+
+class GroupTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        // 테이블 띄우기 직전에!!!실행됨
+        let newGroup : Group = Group(title : "abc", location : "사근동", curPersonNumber : 6, maxPersonNumber : 7, endDate : "2019/05/01")
+        arrGroup.append(newGroup)
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        // 테이블 뷰 개수!!
+        return arrGroup.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Group", for: indexPath)
+
+        let groupForTheRow = arrGroup[indexPath.row]
+        //Indexpath를 통해 접근한다!!!!!
+        cell.textLabel?.text = groupForTheRow.title
+        cell.detailTextLabel?.text =
+            String(groupForTheRow.curPersonNumber) + "/" + String(groupForTheRow.maxPersonNumber) + " " + String(groupForTheRow.location)
+        cell.imageView?.image = UIImage(named: "abc")
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
