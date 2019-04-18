bbe34848f2c2170767c73b6527c484cabca79106
2015004075 <2015004075@hanyang.ac.kr>
2019-04-18 18:39:17 +0900

    little change

--- TeamFind/GroupTableViewController.swift
+++ TeamFind/GroupTableViewController.swift
@@ -87,14 +87,19 @@ class GroupTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        let dstVC = segue.destination as! DetailViewController
+        let selectedInfo = GroupTableViewController(self.tableView.indexPathForSelectedRow?.row)
+        
+        
+        
     }
-    */
+    
 
 }
